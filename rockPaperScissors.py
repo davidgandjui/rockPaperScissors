607577e2d49e765a59729eaa0a518f9c67407778
davidgandjui <55326298+davidgandjui@users.noreply.github.com>
2020-02-28 18:28:54 -0600

    Initial commit

---
+++ rockPaperScissors.py
@@ -0,0 +1,12 @@
+import random
+
+keepPlaying = True
+
+while(keepPlaying == True):
+    print("Welcome to the rock paper scissors!")
+    print("Best 2 out of 3, press q to quit.")
+
+playerScore = 0
+computerScore = 0
+
+while(playScore < 2 and computerScore > 2)
